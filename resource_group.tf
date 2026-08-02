7ef1bf118e73269da3dca4829910379d3cd3187f
Ritesh Sharma <er.ritesh2@gmail.com>
2026-08-02 17:23:43 +0530

    New rg-1 added in feature/first

--- resource_group.tf
+++ resource_group.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "ravi_rg" {
   name     = "ravinder-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "ravi_rg" {
+  name     = "rg-first"
+  location = "West Europe"
+}
