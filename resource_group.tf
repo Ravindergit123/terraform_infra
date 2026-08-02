79f7f6de33f9895c4d77f93b59cd986301e7b059
Ravinder Kumar <ravinderkumarairtel4@gmail.com>
2026-08-02 16:58:24 +0530

    stg_code

--- resource_group.tf
+++ resource_group.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "ravi_rg" {
   name     = "ravinder-rg"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "ravi_sa" {
+  name                     = "ravindersa"
+  resource_group_name      = azurerm_resource_group.ravi_rg.name
+  location                 = azurerm_resource_group.ravi_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
