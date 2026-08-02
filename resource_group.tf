a7b5e4a32eb6d0cf7c6fc56ea93d4541892007ad
Ritesh Sharma <82014945+Riteshatri@users.noreply.github.com>
2026-08-02 17:39:25 +0530

    Feature/first (#5)
    * New rg-1 added in feature/first
    
    * New rg-1 added in feature/second
    
    * New rg-1 added in feature/third

--- resource_group.tf
+++ resource_group.tf
@@ -2,3 +2,19 @@ resource "azurerm_resource_group" "ravi_rg" {
   name     = "ravinder-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "ravi_rg" {
+  name     = "rg-first"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "ravi_rg" {
+  name     = "rg-second"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "ravi_rg" {
+  name     = "rg-third"
+  location = "West Europe"
+}
+
